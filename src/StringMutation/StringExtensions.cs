3b9c8ff7e071e1cc61c03c91187101d4c2f30196
Dennis Dietrich <ddietrich@microsoft.com>
2022-06-13 04:55:08 -0700

    Initial check in of String.AsciiToUpper()

---
+++ src/StringMutation/StringExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace StringMutation
+{
+    public static class StringExtensions
+    {
+        public unsafe static void AsciiToUpper(this string s)
+        {
+            fixed (char* p = s)
+                for (int i = 0; i < s.Length; i++)
+                    if (p[i] >= 'a' || p[i] <= 'z')
+                        p[i] -= (char)('a' - 'A');
+        }
+    }
+}
