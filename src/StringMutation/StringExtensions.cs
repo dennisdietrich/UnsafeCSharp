141f6970e578230b4c583e1a8d9f622cb3e29e53
Dennis Dietrich <ddietrich@microsoft.com>
2022-06-17 14:46:00 -0700

    Initial check in of slide deck

--- src/StringMutation/StringExtensions.cs
+++ src/StringMutation/StringExtensions.cs
@@ -2,10 +2,13 @@
 {
     public static class StringExtensions
     {
+        // Method is declared unsafe
         public unsafe static void AsciiToUpper(this string s)
         {
+            // Pins string and gets address of first element of char array
             fixed (char* p = s)
                 for (int i = 0; i < s.Length; i++)
+                    // Pointer element access
                     if (p[i] >= 'a' && p[i] <= 'z')
                         p[i] -= (char)('a' - 'A');
         }
@@ -25,6 +28,19 @@
                     ptr++;
                 }
             }
+        }
+
+        public unsafe static void AlsoBrokenAsciiToUpper(this string s)
+        {
+            char* ptr;
+
+            fixed (char* p = s)
+                ptr = p;
+
+            // string s is not pinned anymore, ptr may or may not point to it
+            for (int i = 0; i < s.Length; i++)
+                if (ptr[i] >= 'a' && ptr[i] <= 'z')
+                    ptr[i] -= (char)('a' - 'A');
         }
     }
 }
--- src/StringMutationConsole/Program.cs
+++ src/StringMutationConsole/Program.cs
@@ -2,5 +2,5 @@
 
 string s = "Hello, World!";
 s.AsciiToUpper();
-Console.WriteLine(s);
+Console.WriteLine(s); // prints "HELLO, WORLD!"
 Console.ReadKey(true);
