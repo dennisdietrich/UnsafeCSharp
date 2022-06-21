81b6361c56c517849ac84747ca33b1707aed2725
Dennis Dietrich <ddietrich@microsoft.com>
2022-06-21 13:49:03 -0700

    Added heap allocations with BCL classes

---
+++ src/MemoryManagement/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.InteropServices;
+
+// The following code is not unsafe:
+// Allocate memory from default heap
+IntPtr intPtr = Marshal.AllocHGlobal(1024);
+// Free memory
+Marshal.FreeHGlobal(intPtr);
+
+unsafe
+{
+    // Allocate heap memory using C standard library function malloc()
+    void* ptr = NativeMemory.Alloc(1024);
+    // Free heap memory using C standard library function free()
+    NativeMemory.Free(ptr);
+}
