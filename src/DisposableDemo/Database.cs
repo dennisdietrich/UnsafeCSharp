e20fed137b83524486424f8666f9b3c6beeda091
Dennis Dietrich <ddietrich@microsoft.com>
2022-06-22 13:50:59 -0700

    Final check in of rev. 1 (I think)

--- src/DisposableDemo/Database.cs
+++ src/DisposableDemo/Database.cs
@@ -10,12 +10,16 @@
                 throw new Exception("Failed to open database.");
         }
 
+        // Look mom! No finalizer!
+
         public unsafe void Execute(string sql)
         {
+            // Look mom! No 'disposed' check!
             if (Sqlite.Execute(_databaseHandle, sql, null, null, null) != 0)
                 throw new Exception("Script execution failed.");
         }
 
+        // No need for overloads since the class is sealed.
         public void Dispose()
         {
             if (_databaseHandle?.IsInvalid == false)
