a3d5526f44bc689bedd85ab3126c2fdbc81fb8f9
Dennis Dietrich <ddietrich@microsoft.com>
2022-06-21 17:04:59 -0700

    Added P/Invoke slides

---
+++ src/MemoryManagement/Kernel32.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.InteropServices;
+
+namespace MemoryManagement
+{
+    internal static class Kernel32
+    {
+        internal const string DllName = "Kernel32";
+
+        internal static readonly IntPtr HeapHandle = GetProcessHeap();
+
+        [DllImport(DllName)]
+        internal static extern IntPtr GetProcessHeap();
+
+        [DllImport(DllName)]
+        internal static unsafe extern void* HeapAlloc(IntPtr hHeap, uint dwFlags, nuint dwBytes);
+
+        [DllImport(DllName)]
+        internal static unsafe extern int HeapFree(IntPtr hHeap, uint dwFlags, void* lpMem);
+    }
+}
--- src/MemoryManagement/Program.cs
+++ src/MemoryManagement/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System.Runtime.InteropServices;
 
+using static MemoryManagement.Kernel32;
+
 // The following code is not unsafe:
 // Allocate memory from default heap
 IntPtr intPtr = Marshal.AllocHGlobal(1024);
@@ -12,4 +14,14 @@ unsafe
     void* ptr = NativeMemory.Alloc(1024);
     // Free heap memory using C standard library function free()
     NativeMemory.Free(ptr);
+}
+
+unsafe
+{
+    void* ptr = HeapAlloc(HeapHandle, 0, 1024);
+
+    if (ptr == null)
+        throw new OutOfMemoryException();
+
+    HeapFree(HeapHandle, 0, ptr);
 }
