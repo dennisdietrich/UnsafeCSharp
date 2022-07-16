80185f297dcbd511cf49449896d9c45229949c6f
Dennis Dietrich <ddietrich@microsoft.com>
2022-07-16 15:39:04 -0700

    Added CollectedCallbackDelegate project

---
+++ src/CollectedCallbackDelegate/CallbackFromNative.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.InteropServices;
+
+namespace CollectedCallbackDelegate
+{
+    internal static class CallbackFromNative
+    {
+        [DllImport("CallbackFromNative.dll")]
+        internal extern static void SetCallback(Action action);
+
+        [DllImport("CallbackFromNative.dll")]
+        internal extern static void CallMeMaybe();
+    }
+}
---
+++ src/CollectedCallbackDelegate/Program.cs
@@ -0,0 +1,15 @@
+﻿using static CollectedCallbackDelegate.CallbackFromNative;
+
+static void SetupAndTriggerNativeCallback()
+{
+    static void SayHello() => Console.WriteLine("Hello, .NET!");
+
+    SetCallback(SayHello);
+    CallMeMaybe();
+}
+
+SetupAndTriggerNativeCallback();
+// Do lots of stuff here...
+GC.Collect();
+GC.WaitForPendingFinalizers();
+CallMeMaybe();
